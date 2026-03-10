d6e8b55fec1dbb42efd290cc29ee8378f6c18bf5
antemaandrianarivosoa-sys <antemaandrianarivosoa@gmail.com>
2026-03-10 13:55:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Toggle theme
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    btn.textContent = document.body.classList.contains("dark-mode") 
+        ? "☀️ Mode Clair" 
+        : "🌙 Mode Sombre";
+});
 
